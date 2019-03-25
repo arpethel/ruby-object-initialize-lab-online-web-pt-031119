6305104f46c9a6685297a2e0a2482b4baa27342c
Ryan Pethel <arpethel@gmail.com>
2019-03-25 09:23:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,11 @@
 class Dog
   def initialize(breed)
     @breed = breed
-    @breed = "Mutt" unless breed != nil
+    @breed = "Mutt" if breed == nil
+
+    # if @breed == nil
+    #   @breed = "Mutt"
+    # end
   end
 
   def breed=(breed)
