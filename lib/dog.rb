091f6c463cbf927c1bb90387edab7eca7407bfc0
Ryan Pethel <arpethel@gmail.com>
2019-03-25 09:24:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,11 +8,11 @@ class Dog
     # end
   end
 
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
+  # def breed=(breed)
+  #   @breed = breed
+  # end
+  #
+  # def breed
+  #   @breed
+  # end
 end
